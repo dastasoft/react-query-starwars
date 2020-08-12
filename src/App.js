03bac2b4b81401a2e4a2312ff6f5f7923055e2bb
dastasoft <dastasoft@gmail.com>
2020-08-12 10:23:24 +0200

    fixed title

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <>
       <div className="App">
-        <h1>Info</h1>
+        <h1>Star Wars Info</h1>
         <Navbar setPage={setPage} />
         <div className="content">
           {page === 'planets' ? <Planets /> : <People />}
